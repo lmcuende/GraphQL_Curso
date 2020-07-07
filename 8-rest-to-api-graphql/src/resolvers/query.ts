ae053e6a4e28a448aac994eb415c91adcae0c405
Luis Miguel Cuende <lmcuende@gmail.com>
2020-07-07 12:24:45 +0200

    Connecting Seasons
    Getting data from API F1 of Seasons

--- 8-rest-to-api-graphql/src/resolvers/query.ts
+++ 8-rest-to-api-graphql/src/resolvers/query.ts
@@ -1,8 +1,13 @@
 import { IResolvers } from 'graphql-tools';
+import { dataSources } from '../data/index';
 
 const resolvers: IResolvers = {
     Query: {
-        
+        async seasonsList(_: void, __: any, { dataSources } ) {
+            return await  dataSources.seasons.getSeasons().then(
+                (data: any) => data.MRData.SeasonTable.Seasons
+            );
+        }
     }
 };
 
