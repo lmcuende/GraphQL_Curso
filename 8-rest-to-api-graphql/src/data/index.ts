8af6861ff3b870b578762d79f4a130b53956684d
Luis Miguel Cuende <lmcuende@gmail.com>
2020-07-10 08:30:10 +0200

    Exercises
    Exercises completed

--- 8-rest-to-api-graphql/src/data/index.ts
+++ 8-rest-to-api-graphql/src/data/index.ts
@@ -1,9 +1,11 @@
 import { RacesData } from './data-races';
 import { SeasonsData } from './data-seasons';
 import { DriversData } from './data-drivers';
+import { CircuitsData } from './data-circuits';
 
 export const dataSources = {
     SeasonsData,
     RacesData,
-    DriversData
+    DriversData,
+    CircuitsData
 }
